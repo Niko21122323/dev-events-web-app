bb58acb2812422570df0d7454b10b8d9d1ebe63b
Nikola Stojanovski <nikola211223232529321@gmail.com>
2025-11-02 17:22:57 +0100

    implementing database models

--- app/page.tsx
+++ app/page.tsx
@@ -12,10 +12,8 @@ export default function Home() {
         Hackatons, Meetups and Conferences. All in One Place
       </p>
       <ExploreBtn />
-
       <div className="mt-20 space-y-7">
         <h3>Featured Events</h3>
-
         <ul className="events">
           {events.map((event) => (
             <li key={event.title} className="list-none">
